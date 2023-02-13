b614fc16fddf68c6bb2f5ff150be45a8d24efee4
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-02-14 01:23:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,17 +12,15 @@ export class App extends Component {
     filter: '',
   };
 
-  componentDidUpdate() {
+  componentDidMount() {
+    this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    //console.log(prevProps, prevState);
     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
   }
 
-  componentDidMount() {
-    if (JSON.parse(localStorage.getItem('contacts'))) {
-      this.setState({
-        contacts: [JSON.parse(localStorage.getItem('contacts'))],
-      });
-    }
-  }
   addContactHandler = evt => {
     evt.preventDefault();
     const { name, number } = evt.target.elements;
